0ae0fcc0ce3c6ab5aaaf39c4629467ebd0aa5a5b
Kerry Buckley <kerryjbuckley@gmail.com>
2016-08-05 10:54:43 +0100

    Store values as new left or right nodes when root already has a value

--- lib/btree.ex
+++ lib/btree.ex
@@ -1,8 +1,15 @@
 defmodule BTree do
   defstruct [:key, :value, :left, :right]
 
-  def insert(tree, key, value) do
+  def insert(tree = %BTree{key: nil}, key, value) do
     %{tree | key: key, value: value}
+  end
+  def insert(tree, key, value) do
+    if key < tree.key do
+      %{tree | left: %BTree{key: key, value: value}}
+    else
+      %{tree | right: %BTree{key: key, value: value}}
+    end
   end
 
   def search(tree, key) do
--- test/btree_test.exs
+++ test/btree_test.exs
@@ -5,18 +5,35 @@ defmodule BTreeTest do
   describe "BTree, as a whole" do
     test "allows insertion and search" do
       tree = %BTree{}
-              |> BTree.insert("one", 1)
-              |> BTree.insert("two", 2)
-              |> BTree.insert("three", 3)
-      assert BTree.search(tree, "two") == 2
+              |> BTree.insert("bravo", 1)
+              |> BTree.insert("alpha", 2)
+              |> BTree.insert("charlie", 3)
+              |> BTree.insert("delta", 4)
+      assert BTree.search(tree, "charlie") == 3
     end
   end
 
   describe "An empty BTree" do
     test "stores the first inserted value itself" do
       tree = %BTree{}
-              |> BTree.insert("one", 1)
-      assert {tree.key, tree.value} == {"one", 1}
+              |> BTree.insert("alpha", 1)
+      assert {tree.key, tree.value} == {"alpha", 1}
+    end
+  end
+
+  describe "A BTree with a value" do
+    setup do
+      [tree: %BTree{key: "bravo", value: 1}]
+    end
+
+    test "stores values with keys less than its own in its left node", context do
+      tree = context[:tree] |> BTree.insert("alpha", 2)
+      assert tree.left.key == "alpha"
+    end
+
+    test "stores values with keys greater than its own in its right node", context do
+      tree = context[:tree] |> BTree.insert("charlie", 2)
+      assert tree.right.key == "charlie"
     end
   end
 end
