ca7dbf094c21e8ee021f30d1d069089d2b9bf626
Kerry Buckley <kerryjbuckley@gmail.com>
2016-08-06 14:41:14 +0100

    Refactor to guards

--- lib/btree.ex
+++ lib/btree.ex
@@ -4,21 +4,13 @@ defmodule BTree do
   def insert(tree = nil, key, value), do: %BTree{key: key, value: value}
   def insert(tree = %BTree{key: nil}, key, value), do: %{tree | key: key, value: value}
   def insert(tree = %BTree{key: key}, key, value), do: %{tree | value: value}
-  def insert(tree, key, value) do
-    if key < tree.key do
-      %{tree | left: BTree.insert(tree.left, key, value)}
-    else
-      %{tree | right: BTree.insert(tree.right, key, value)}
-    end
+  def insert(tree = %BTree{key: our_key}, key, value) when key < our_key do
+    %{tree | left: BTree.insert(tree.left, key, value)}
   end
+  def insert(tree, key, value), do: %{tree | right: BTree.insert(tree.right, key, value)}
 
   def search(nil, _), do: nil
   def search(%BTree{key: key, value: value}, key), do: value
-  def search(tree, key) do
-    if key < tree.key do
-      search tree.left, key
-    else
-      search tree.right, key
-    end
-  end
+  def search(tree = %BTree{key: our_key}, key) when key < our_key, do: search tree.left, key
+  def search(tree, key), do: search tree.right, key
 end
